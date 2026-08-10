a1eba69693d1b5cac5e3eb8234bfe7b3bd1614e7
Vishal Kumar <67638997+vishal8218@users.noreply.github.com>
2026-08-10 11:48:52 +0530

    Comment out FirebaseUtils class code

--- automation-framework/src/main/java/db/FirebaseUtils.java
+++ automation-framework/src/main/java/db/FirebaseUtils.java
@@ -1,63 +1,63 @@
-package db;
+// package db;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+// import java.io.BufferedReader;
+// import java.io.FileInputStream;
+// import java.io.InputStreamReader;
+// import java.io.OutputStream;
+// import java.net.HttpURLConnection;
+// import java.net.URL;
+// import java.util.HashMap;
+// import java.util.Map;
+// import java.util.UUID;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.Firestore;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.cloud.FirestoreClient;
+// import com.google.auth.oauth2.GoogleCredentials;
+// import com.google.cloud.firestore.Firestore;
+// import com.google.firebase.FirebaseApp;
+// import com.google.firebase.FirebaseOptions;
+// import com.google.firebase.cloud.FirestoreClient;
 
-import utils.ConfigReader;
+// import utils.ConfigReader;
 
-public class FirebaseUtils {
+// public class FirebaseUtils {
 
-	ConfigReader configReader;
-	 private Firestore db ;
+// 	ConfigReader configReader;
+// 	 private Firestore db ;
 
-	 public  FirebaseUtils() {
-		 configReader=new ConfigReader();
-	        try {
-	            FileInputStream serviceAccount = new FileInputStream(configReader.getCred());
+// 	 public  FirebaseUtils() {
+// 		 configReader=new ConfigReader();
+// 	        try {
+// 	            FileInputStream serviceAccount = new FileInputStream(configReader.getCred());
 
-	            FirebaseOptions options = FirebaseOptions.builder()
-	            	    .setCredentials(GoogleCredentials.fromStream(new FileInputStream(configReader.getCred())))
-	            	    .setDatabaseUrl(configReader.getDbUrl())
-	            	    .build();
+// 	            FirebaseOptions options = FirebaseOptions.builder()
+// 	            	    .setCredentials(GoogleCredentials.fromStream(new FileInputStream(configReader.getCred())))
+// 	            	    .setDatabaseUrl(configReader.getDbUrl())
+// 	            	    .build();
 
-	            if (FirebaseApp.getApps().isEmpty()) {
-	                FirebaseApp.initializeApp(options);
+// 	            if (FirebaseApp.getApps().isEmpty()) {
+// 	                FirebaseApp.initializeApp(options);
 	              
 	                
-	            }
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	    }
-	  public void saveData(String userEmail,String pass) throws Exception
-	    {
-	    	 db =  (Firestore)FirestoreClient.getFirestore();
+// 	            }
+// 	        } catch (Exception e) {
+// 	            e.printStackTrace();
+// 	        }
+// 	    }
+// 	  public void saveData(String userEmail,String pass) throws Exception
+// 	    {
+// 	    	 db =  (Firestore)FirestoreClient.getFirestore();
 
-		        System.out.println("SAVE DATA  "+userEmail);
-	  	  		    Map<String, Object> credentialsData = new HashMap<>();  
-	  	  		    String uuid= UUID.randomUUID().toString();
-	  	  	  	credentialsData .put("userId",uuid);
-  	  		credentialsData .put("email", userEmail);
-	  	  	   credentialsData.put("password", pass);
-	 		    db.collection("CoachData").document(uuid).set(credentialsData);
-//			 
+// 		        System.out.println("SAVE DATA  "+userEmail);
+// 	  	  		    Map<String, Object> credentialsData = new HashMap<>();  
+// 	  	  		    String uuid= UUID.randomUUID().toString();
+// 	  	  	  	credentialsData .put("userId",uuid);
+//   	  		credentialsData .put("email", userEmail);
+// 	  	  	   credentialsData.put("password", pass);
+// 	 		    db.collection("CoachData").document(uuid).set(credentialsData);
+// //			 
 			
 	  		    
-	    }
+// 	    }
 	  
 	 
 
-}
+// }
