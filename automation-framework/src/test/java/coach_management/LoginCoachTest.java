99d3a36c6f8e3a4c88a557ed10f9c92cf432139b
Vishal Kumar <67638997+vishal8218@users.noreply.github.com>
2026-08-10 15:09:47 +0530

    Update LoginCoachTest.java

--- automation-framework/src/test/java/coach_management/LoginCoachTest.java
+++ automation-framework/src/test/java/coach_management/LoginCoachTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
 import org.testng.annotations.Test;
 
 import drivermanager.DriverFactory;
