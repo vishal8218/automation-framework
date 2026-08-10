4c98a444f968e0c8ae0ae1411ae5d06f823482eb
Vishal Kumar <67638997+vishal8218@users.noreply.github.com>
2026-08-10 11:43:34 +0530

    Remove unused FirebaseUtils import
    Removed unused import for FirebaseUtils.

--- automation-framework/src/main/java/workout/ExcerciseCreation.java
+++ automation-framework/src/main/java/workout/ExcerciseCreation.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import db.FirebaseUtils;
 
 public class ExcerciseCreation {
 
