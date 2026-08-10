ce47e3ffe76454de03c4f998a1a498e293c99cda
Vishal Kumar <67638997+vishal8218@users.noreply.github.com>
2026-08-10 16:18:28 +0530

    Refactor TestAddClient for improved browser handling

--- automation-framework/src/test/java/usermangement/TestAddClient.java
+++ automation-framework/src/test/java/usermangement/TestAddClient.java
@@ -2,9 +2,8 @@ package usermangement;
 
 import java.io.IOException;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -19,40 +18,41 @@ public class TestAddClient {
 
 	private WebDriver driver;
 	private ConfigReader configReader;
-	private String browser="chrome";
-	private long userName=System.currentTimeMillis();
-    private String	email="user" + userName + "@yopmail.com";
-    private String fName="firstName";
-private String lName="lastName";
-private String phone="98701189904";
-private int age=35;
-private int height =172, weight =90;
-private String goals[]=new String[]{"Loose Weight","Build Muscle"};
-	@BeforeTest
-	public void openBrowser()
-	{
-		DriverManager.setDriver(		DriverFactory.createDriver(browser));
-		this.configReader =new ConfigReader();
-		this.driver=DriverManager.getDriver();
-		this.driver.get( configReader.getCoachPortfolioLink());
-         this.driver.manage().window().maximize();
-	}
-	@Test
-	public void addClient() throws InterruptedException, IOException
-	{
-		AddClientWeb addClientWeb=new AddClientWeb(this.driver);
-		addClientWeb.basicDetail(phone, fName, lName, email);
-	addClientWeb.enterPhysicalDetail("female",age, "veg");
-	addClientWeb.enterPhysicalDetail(height, weight, goals);
-	addClientWeb.enterDietPreferences("VEG");
-	String result = addClientWeb.payment("monthly");
-	Assert.assertEquals(result, "Client Registered Successfully", "Client registration failed");
-	}
-	
+	private String browser = "chrome";
+	private long userName = System.currentTimeMillis();
+	private String email = "user" + userName + "@yopmail.com";
+	private String fName = "firstName";
+	private String lName = "lastName";
+	private String phone = "98701282904";
+	private int age = 35;
+	private int height = 172, weight = 90;
+	private String goals[] = new String[] { "Loose Weight", "Build Muscle" };
 
-@AfterTest
-public void closeBrowser()
-{
-	this.driver.quit();
-}
+	@BeforeTest
+	public void openBrowser() {
+		this.configReader = new ConfigReader();
+		ChromeOptions options = new ChromeOptions();
+
+		// Use the same CI-aware driver setup as the rest of the suite
+		DriverManager.setDriver(DriverFactory.createDriverPermisson(browser, options));
+		this.driver = DriverManager.getDriver();
+		this.driver.manage().window().maximize();
+		this.driver.get(configReader.getCoachPortfolioLink());
+	}
+
+	@Test
+	public void addClient() throws InterruptedException, IOException {
+		AddClientWeb addClientWeb = new AddClientWeb(this.driver);
+		addClientWeb.basicDetail(phone, fName, lName, email);
+		addClientWeb.enterPhysicalDetail("female", age, "veg");
+		addClientWeb.enterPhysicalDetail(height, weight, goals);
+		addClientWeb.enterDietPreferences("VEG");
+		String result = addClientWeb.payment("monthly");
+		Assert.assertEquals(result, "Client Registered Successfully", "Client registration failed");
+	}
+
+	@AfterTest
+	public void closeBrowser() {
+		DriverManager.getDriver().quit();
+	}
 }
